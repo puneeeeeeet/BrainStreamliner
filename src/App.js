8f9965a0da801ae1be1626ca6ac6d199b59813bf
puneeeeeeet <puneetchhabra1607@gmail.com>
2024-07-03 05:23:45 +0530

    hoempage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Navbar from "./Components/Navbar";
+import Slider from './Components/Slider'
+import Intro from './Components/Introduction'
+
 function App() {
   return (
-    <div >
+    <div className=" ">
       <Navbar></Navbar>
+      <Slider></Slider>
+      <Intro></Intro>
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,24 +14,27 @@ const Navbar = () => {
   // Array containing navigation items
   const navItems = [
     { id: 1, text: 'Home' },
-    { id: 2, text: 'Company' },
-    { id: 3, text: 'Resources' },
-    { id: 4, text: 'About' },
-    { id: 5, text: 'Contact' },
+    { id: 2, text: 'About us' },
+    { id: 3, text: 'Self Assessment' },
+    { id: 4, text: 'Workshop' },
+    { id: 5, text: 'Group Therapies' }, 
+    { id: 6, text: 'Blogs' },
+    { id: 7, text: 'Offer' },
+    { id: 8, text: 'Contact us' },
   ];
 
   return (
-    <div className='bg-[#FFF8DB] flex justify-between items-center h-24  mx-auto px-4 text-white'>
+    <div className='bg-[#7D8ABC] flex justify-between items-center h-24  mx-auto px-4 text-white'>
 
       <img className=' ' src={logo} alt=""  height={80} width={75}/>
-      <h1 className='w-full text-3xl font-bold text-[#304463] font-mono ml-4'>Brain Streamliner</h1>
+      <a href='/' className='w-full text-3xl font-bold text-[#FFF8DB] font-mono ml-4'>Brain Streamliner</a >
 
       {/* Desktop Navigation */}
       <ul className='hidden md:flex'>
         {navItems.map(item => (
           <li
             key={item.id}
-            className='p-4 hover:bg-[#939185] rounded-xl m-2 text-[#304463] cursor-pointer duration-300 text-xl hover:text-black'
+            className='px-5 py-4 hover:bg-[#304463] rounded-xl m-2 text-[#FFF8DB] cursor-pointer duration-300 text-nowrap hover:text-[#FFF8DB]'
           >
             {item.text}
           </li>
@@ -47,18 +50,18 @@ const Navbar = () => {
       <ul
         className={
           nav
-            ? 'fixed md:hidden left-0 top-0 w-[70%] h-full  bg-[#EEEDEB] ease-in-out duration-500'
+            ? 'fixed md:hidden left-0 top-0 w-[70%] h-full  bg-[#7D8ABC] ease-in-out duration-500'
             : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
         }
       >
         {/* Mobile Logo */}
-        <h1 className='w-full text-2xl font-bold text-[#E6E6FA] m-4'>Brain Streamliner</h1>
+        <h1 className='w-full text-2xl font-bold text-[#FFF8DB] m-4'>Brain Streamliner</h1>
 
         {/* Mobile Navigation Items */}
         {navItems.map(item => (
           <li
             key={item.id}
-            className='p-4  rounded-xl hover:bg-[#E6E6FA] duration-300 hover:text-black cursor-pointer '
+            className='p-4  rounded-xl hover:bg-[#304463] duration-300 hover:text-[#FFF8DB] cursor-pointer '
           >
             {item.text}
           </li>
