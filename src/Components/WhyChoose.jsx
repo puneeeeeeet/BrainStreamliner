6e470f98d9814cb94a2c6a94dd1424ae123d9604
puneeeeeeet <puneetchhabra1607@gmail.com>
2024-08-07 10:55:22 +0530

    choose us layout

---
+++ src/Components/WhyChoose.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const WhyChoose = () => {
+  return (
+    <div>
+            <div className='flex items-center justify-center text-4xl font-serif underline underline-offset-2 pt-10'>
+                    Why Choose Us
+            </div>
+
+            <div className='flex items-center justify-center p-20'>
+            <div className='text-xl  text-center w-72 '>
+            Psychopedia promises you to bring the best teaching methods and enhance your well-being
+            </div>
+            </div>
+            
+            <div className='flex items-center justify-center flex-col sm:flex-row px-8 gap-2'>
+                <div className='sm:w-80 w-full  h-96 flex items-center justify-center  flex-col border-2 border-black rounded-md'>
+                    <div className='w-24 h-24 bg-black '>Image
+                            {/* IMAGE */}
+                    </div>
+                    <div className='text-2xl py-5 '>Guest Lectures
+                            {/* HEADING */}
+                    </div>
+                    <div className='text-xl text-center px-2'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sof type and 
+                        </div>
+                </div>
+
+                <div className='sm:w-80 w-full  h-96 flex items-center justify-center  flex-col border-2 border-black rounded-md'>
+                    <div className='w-24 h-24 bg-black '>
+                            {/* IMAGE */}
+                    </div>
+                    <div className='text-2xl py-5 '>Smart Class Room
+                            {/* HEADING */}
+                    </div>
+                    <div className='text-xl text-center px-2'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sof type and 
+                        </div>
+                </div>
+
+                <div className='sm:w-80 w-full  h-96 flex items-center justify-center  flex-col border-2 border-black rounded-md'>
+                    <div className='w-24 h-24 bg-black '>
+                            {/* IMAGE */}
+                    </div>
+                    <div className='text-2xl py-5 '> Training and Workshops
+                            {/* HEADING */}
+                    </div>
+                    <div className='text-xl text-center px-2'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sof type and 
+                        </div>
+                </div>
+
+                <div className='sm:w-80 w-full  h-96 flex items-center justify-center  flex-col border-2 border-black rounded-md'>
+                    <div className='w-24 h-24 bg-black '>
+                            {/* IMAGE */}
+                    </div>
+                    <div className='text-2xl py-5 '> Councelling & Guidence
+                            {/* HEADING */}
+                    </div>
+                    <div className='text-xl text-center px-2'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sof type and 
+                        </div>
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default WhyChoose
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Slider from '../Components/Slider';
 import Intro from '../Components/Introduction';
+import WhyChoose from '../Components/WhyChoose';
 
 const Home = () => {
   return (
     <div>
         <Slider></Slider>
         <Intro></Intro>
+        <WhyChoose></WhyChoose>
     </div>
   )
 }
