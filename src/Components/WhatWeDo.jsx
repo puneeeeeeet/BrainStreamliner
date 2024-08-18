57018bb99f0ab19079080f882ab5e875e5e67f47
puneeeeeeet <puneetchhabra1607@gmail.com>
2024-08-18 14:31:18 +0530

    cards drawn

--- src/Components/WhatWeDo.jsx
+++ src/Components/WhatWeDo.jsx
@@ -17,7 +17,7 @@ const WhatWeDo = () => {
   }, []);
   return (
     <>
-    <div className='w-screen h-[200vh] sm:h-[150vh] flex sm:flex-row flex-col text-blue  bg-white relative '>
+    <div className='w-screen h-[400vh] sm:h-[250vh] flex sm:flex-row flex-col text-blue  bg-white relative pb-44'>
         <div className='  sm:w-1/2 w-full sm:flex sm:items-center  flex-col gap-5'>
         
         <div className='sticky top-0 sm:top-48  sm:ml-40 sm:pb-40 mt-40 sm:w-full w-11/12 ml-4' >
@@ -37,25 +37,63 @@ const WhatWeDo = () => {
 
         
         {/* Left container */}
-        <div className='sm:w-1/2 h-full flex items-center  flex-col'>
+        <div className='sm:w-1/2 h-full flex items-center justify-center  flex-col text-white mt-20 relative'>
         
         {/* first card */}
-          <div className='items-end flex sticky mt-16 sm:mt-96 top-32 sm:top-40 border-2 border-black h-20 w-10/12 mb-32'>
-1
-
+        <div className=' flex sticky mt-16 sm:mt-48 top-32 sm:top-52  w-10/12  mb-24 sm:mb-32 items-center justify-center'>
+        <div className=' h-20 bg-blue w-1/2 rounded-xl'>        
+        Anxiety
           </div>
+        </div>
+          
           {/* Second Card */}
-
-          <div className='items-end flex sticky top-36 sm:top-44 border-2 border-black h-20 w-10/12 mb-32'>
-2
+          <div className=' flex sticky top-36 sm:top-56   w-10/12 mb-24 sm:mb-32 items-center justify-center'>  
+         <div className=' w-1/2 h-20 bg-[#FFC7ED] rounded-xl'>
+         Depression
+         </div> 
           </div>
-          <div className='items-end flex sticky top-40 sm:top-48 border-2 border-black h-20 w-10/12 mb-32'>
-3
 
+          {/* Third Card */}
+          <div className=' flex sticky top-40 sm:top-60   h-[100vh] w-10/12 mb-24 sm:mb-32 items-center justify-center'>  
+         <div className=' w-1/2 h-20 bg-[#FFF8DB] rounded-xl'>
+         Depression
+         </div> 
           </div>
-          <div className='items-end flex sticky top-44 sm:top-52 border-2 border-black h-20 w-10/12 mb-32'>
-4 
-
+          {/* Fourth Card */}
+          <div className=' flex sticky top-44 sm:top-64  h-[50vh] w-10/12 mb-24 sm:mb-32 items-center justify-center'>  
+         <div className=' w-1/2 h-20 bg-[#304463] rounded-xl'>
+         Depression
+         </div> 
+          </div>
+          {/* Fifth card */}
+          <div className=' flex sticky top-48 sm:top-64  h-[50vh] w-10/12 mb-24 sm:mb-32 items-center justify-center'>  
+         <div className=' w-1/2 h-20 bg-[#FF8A8A] rounded-xl'>
+         Depression
+         </div> 
+          </div>
+          {/* Sixth Card */}
+          <div className=' flex sticky top-36 sm:top-64  h-[50vh] w-10/12 mb-24 sm:mb-28 items-center justify-center'>  
+         <div className=' w-1/2 h-20 bg-[#FFAD60] rounded-xl'>
+         Depression
+         </div> 
+          </div>
+          {/* Seventh Card */}
+          <div className=' flex sticky top-36 sm:top-72  h-[50vh] w-10/12 mb-24 sm:mb-28 items-center justify-center'>  
+         <div className=' w-1/2 h-20 bg-[#A2CA71] rounded-xl'>
+         Depression
+         </div> 
+          </div>
+          {/* Eigth Card */}
+          <div className=' flex sticky top-36 sm:top-72  h-[50vh] w-10/12 mb-24 sm:mb-28 items-center justify-center'>  
+         <div className=' w-1/2 h-20 bg-[#BB9AB1] rounded-xl'>
+         Depression
+         </div> 
+          </div>
+          {/* Ninth Card */}
+          <div className=' flex sticky top-36 sm:top-72  h-[50vh] w-10/12 mb-60 items-center justify-center'>  
+         <div className=' w-1/2 h-20 bg-blue rounded-xl'>
+         Depression
+         </div> 
           </div>
               {/* Ends Here */}
               </div>
